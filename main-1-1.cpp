88c0c1dc4bc93509ca62dd18dbcf7e85579089be
Tuongvy88 <109933390+Tuongvy88@users.noreply.github.com>
2022-09-07 14:44:40 +0930

    Update main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -8,6 +8,6 @@ int main()
     Cage cage("Vy", 35);
    
     std::cout << "Occupant name: " << cage.get_name() << std::endl;
-    std::cout << "ID Number: " << cage.get_ID_number() << std::endl;
+    std::cout << "ID Number: " << cage.get_IDnum() << std::endl;
     return 0;
 }
